5a5de883fea90122b5c3bf242bf7dbb77697a39b
Crystal <crystal@crystal.local>
2015-10-30 09:53:24 +0800

    Initial Commit

---
+++ IUME/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IUME
+//
+//  Created by Crystal on 15/10/30.
+//  Copyright © 2015年 UME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
